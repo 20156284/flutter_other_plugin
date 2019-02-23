5fe90db45b5e73351501713c3de02170544a119a
will <willzh@live.cn>
2019-02-23 11:35:00 +0800

    Initial commit

---
+++ android/src/main/java/com/willcheung/flutterotherplugin/FlutterOtherPlugin.java
@@ -0,0 +1,25 @@
+package com.willcheung.flutterotherplugin;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** FlutterOtherPlugin */
+public class FlutterOtherPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_other_plugin");
+    channel.setMethodCallHandler(new FlutterOtherPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ ios/Classes/FlutterOtherPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterOtherPlugin.h"
+
+@implementation FlutterOtherPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_other_plugin"
+            binaryMessenger:[registrar messenger]];
+  FlutterOtherPlugin* instance = [[FlutterOtherPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
